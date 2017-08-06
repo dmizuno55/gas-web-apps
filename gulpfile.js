638933a5420636aa0ebde584f9d95e929b747d39
水野大輔 <dmizuno55@gmail.com>
2017-08-06 21:11:52 +0900

    fix

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var gulp = require('gulp');
 var del = require('del');
 var transform = require('gulp-transform');
 var rename = require('gulp-rename');
+var concat = require('gulp-concat');
 
 function toHtml_(content, file) {
   switch(path.extname(file.path)) {
@@ -22,7 +23,15 @@ gulp.task('clean', function(cb) {
 });
 
 gulp.task('gasconv:css', function() {
-  gulp.src('templates/src/css/**/*.css')
+  gulp.src('templates/src/css/vender/*.css')
+    .pipe(concat('vender.css'))
+    .pipe(transform('utf8', toHtml_))
+    .pipe(rename({
+      extname: '.css.html'
+    }))
+    .pipe(gulp.dest('templates/dest/css'));
+
+  gulp.src('templates/src/css/*.css')
     .pipe(transform('utf8', toHtml_))
     .pipe(rename({
       extname: '.css.html'
@@ -31,7 +40,15 @@ gulp.task('gasconv:css', function() {
 });
 
 gulp.task('gasconv:js', function() {
-  gulp.src('templates/src/js/**/*.js')
+  gulp.src('templates/src/js/vender/*.js')
+    .pipe(concat('vender.js'))
+    .pipe(transform('utf8', toHtml_))
+    .pipe(rename({
+      extname: '.js.html'
+    }))
+    .pipe(gulp.dest('templates/dest/js'));
+
+  gulp.src('templates/src/js/*.js')
     .pipe(transform('utf8', toHtml_))
     .pipe(rename({
       extname: '.js.html'
